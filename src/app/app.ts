3670394229a2dc6412f0c5236674629656a84a98
Zacarias Aibbich <zacaab8@gmail.com>
2026-08-13 15:19:32 +0200

    feat: add training programs section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
+import { Programs } from './components/programs/programs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero],
+  imports: [RouterOutlet, Navbar, Hero, Programs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
